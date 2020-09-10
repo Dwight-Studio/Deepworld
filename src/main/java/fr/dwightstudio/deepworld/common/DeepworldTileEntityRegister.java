bc314599ada3398b45ecaf57175220cc9b258de8
Deleranax <reazer18@gmail.com>
2020-09-10 20:19:29 +0200

    Updated to 1.15.2

--- src/main/java/fr/dwightstudio/deepworld/common/DeepworldTileEntityRegister.java
+++ src/main/java/fr/dwightstudio/deepworld/common/DeepworldTileEntityRegister.java
@@ -2,6 +2,7 @@ package fr.dwightstudio.deepworld.common;
 
 import fr.dwightstudio.deepworld.common.tile.TileEntityWoodenFrame;
 import net.minecraft.tileentity.TileEntityType;
+import net.minecraft.util.ResourceLocation;
 import net.minecraftforge.event.RegistryEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 
@@ -11,6 +12,8 @@ public class DeepworldTileEntityRegister {
 
         TileEntityType<TileEntityWoodenFrame> WOODEN_FRAME_TILE_ENTITY_TYPE = TileEntityType.Builder.create(TileEntityWoodenFrame::new,DeepworldBlocks.WOODEN_FRAME).build(null);
         // TileEntityType<TileEntityWoodenPress> WOODEN_PRESS_ENTITY_TYPE = TileEntityType.Builder.create(TileEntityWoodenPress::new, DeepworldBlocks.WOODEN_PRESS).build(null);
+
+        WOODEN_FRAME_TILE_ENTITY_TYPE.setRegistryName(new ResourceLocation(Deepworld.MOD_ID, "wooden_frame"));
 
         event.getRegistry().register(WOODEN_FRAME_TILE_ENTITY_TYPE);
         // event.getRegistry().register(WOODEN_PRESS_ENTITY_TYPE);

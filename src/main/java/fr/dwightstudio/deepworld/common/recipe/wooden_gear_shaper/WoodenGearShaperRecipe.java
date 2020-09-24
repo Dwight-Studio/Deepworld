f447dc9b331cc3fdf69232881f54530c7674b941
GamerMine <47460992+GamerMine@users.noreply.github.com>
2020-09-24 12:51:41 +0200

    Patching machines
    - Added Wooden Case Panel Recipe
    - Fixed Machine recipes

--- src/main/java/fr/dwightstudio/deepworld/common/recipe/wooden_gear_shaper/WoodenGearShaperRecipe.java
+++ src/main/java/fr/dwightstudio/deepworld/common/recipe/wooden_gear_shaper/WoodenGearShaperRecipe.java
@@ -67,7 +67,7 @@ public class WoodenGearShaperRecipe extends DeepworldMachineRecipe {
     }
 
     public ItemStack getResult() {
-        return result;
+        return result.copy();
     }
 
     /**
--- src/main/java/fr/dwightstudio/deepworld/common/recipe/wooden_press/WoodenPressRecipe.java
+++ src/main/java/fr/dwightstudio/deepworld/common/recipe/wooden_press/WoodenPressRecipe.java
@@ -67,7 +67,7 @@ public class WoodenPressRecipe extends DeepworldMachineRecipe {
     }
 
     public ItemStack getResult() {
-        return result;
+        return result.copy();
     }
 
     /**

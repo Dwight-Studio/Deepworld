2d950cb3e165791beb216f09981216cc8e987712
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2023-02-27 22:45:08 +0100

    Final touches, and base files added.

---
+++ src/main/java/fr/dwightstudio/deepworld/DeepworldClient.java
@@ -0,0 +1,10 @@
+package fr.dwightstudio.deepworld;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class DeepworldClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}

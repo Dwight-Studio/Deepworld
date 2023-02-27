20903f239aecadbbbfad20f1553e7aeda3bdcd2d
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2023-02-27 20:59:28 +0100

    Making the project about deepworld

--- src/main/java/fr/dwightstudio/deepworld/ExampleModDeepworld.java
+++ src/main/java/fr/dwightstudio/deepworld/ExampleModDeepworld.java
@@ -9,7 +9,7 @@ public class ExampleModDeepworld implements ModInitializer {
     // This logger is used to write text to the console and the log file.
     // It is considered best practice to use your mod id as the logger's name.
     // That way, it's clear which mod wrote info, warnings, and errors.
-    public static final Logger LOGGER = LoggerFactory.getLogger("Deepworld");
+    public static final Logger LOGGER = LoggerFactory.getLogger("deepworld");
 
     @Override
     public void onInitialize() {

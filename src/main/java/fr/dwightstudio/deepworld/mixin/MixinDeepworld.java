0ebb49d3fc698c92237e322b6c7c0621d787fbbe
joshuadwightred <42832831+joshuadwightred@users.noreply.github.com>
2023-02-28 12:29:31 +0100

    Deleting example usage

--- src/main/java/fr/dwightstudio/deepworld/mixin/MixinDeepworld.java
+++ src/main/java/fr/dwightstudio/deepworld/mixin/MixinDeepworld.java
@@ -1,16 +1,8 @@
 package fr.dwightstudio.deepworld.mixin;
 
-import fr.dwightstudio.deepworld.Deepworld;
 import net.minecraft.client.gui.screen.TitleScreen;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 @Mixin(TitleScreen.class)
 public class MixinDeepworld {
-    @Inject(at = @At("HEAD"), method = "init()V")
-    private void init(CallbackInfo info) {
-        Deepworld.LOGGER.info("Ceci est un test!");
-    }
 }
